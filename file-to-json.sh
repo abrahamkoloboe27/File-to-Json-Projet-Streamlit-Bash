7f78ef0e25e6613ff0a6d657f495cb5017469343
Abraham KOLOBOE <abraham.koloboe@mehriso.com>
2024-09-16 22:11:37 +0100

    Fix Docker image name usage in file-to-json.sh

--- file-to-json.sh
+++ file-to-json.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 if ! [ -x "$(command -v docker)" ]; then
-  echo 'Erreur : Docker n\'est pas installé.' >&2
+  echo "Erreur : Docker n'est pas installé." >&2
   exit 1
 fi
 
-IMAGE_NAME="zach27/file-to-json:latest"
-
 echo "Téléchargement de l'image Docker..."
-docker pull $IMAGE_NAME
+docker pull zach27/file-to-json:latest
 
 echo "Démarrage de l'application Streamlit..."
-docker run -d -p 8501:8501 --name streamlit-app $IMAGE_NAME
+docker run -d -p 8501:8501 --name streamlit-app zach27/file-to-json:latest
 
 echo "L'application Streamlit est maintenant disponible à l'adresse http://localhost:8501"
