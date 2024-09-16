afe5cdde33a056fdf76acbd5066867faff6edbcb
Abraham KOLOBOE <abraham.koloboe@mehriso.com>
2024-09-16 22:36:07 +0100

    Update Streamlit application URL to reflect new port mapping

--- file-to-json.sh
+++ file-to-json.sh
@@ -11,4 +11,4 @@ docker pull zach27/file-to-json:latest
 echo "Démarrage de l'application Streamlit..."
 docker run -d -p 8585:8501 --name streamlit-app zach27/file-to-json:latest
 
-echo "L'application Streamlit est maintenant disponible à l'adresse http://localhost:8501"
+echo "L'application Streamlit est maintenant disponible à l'adresse http://localhost:8585"
