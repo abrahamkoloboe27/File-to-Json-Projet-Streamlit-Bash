123029688a0748208dac6225e5610968d412db09
Abraham KOLOBOE <abraham.koloboe@mehriso.com>
2024-09-16 22:03:23 +0100

    Remove unnecessary checks and streamline Docker image setup in file-to-json.sh

--- file-to-json.sh
+++ file-to-json.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# Vérifier si Docker est installé
 if ! [ -x "$(command -v docker)" ]; then
   echo 'Erreur : Docker n\'est pas installé.' >&2
   exit 1
 fi
 
-# Définir le nom de l'image Docker
 IMAGE_NAME="zach27/file-to-json:latest"
 
-# Tirer l'image depuis Docker Hub
 echo "Téléchargement de l'image Docker..."
 docker pull $IMAGE_NAME
 
-# Démarrer le conteneur avec l'image tirée
 echo "Démarrage de l'application Streamlit..."
 docker run -d -p 8501:8501 --name streamlit-app $IMAGE_NAME
 
